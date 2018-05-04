c05f175c8649ea3b6e03182736720dde8a7d16c6
Mehran <Mehran@users.noreply.github.com>
2018-05-04 17:02:03 +0430

    Added Brew Cask to install software

---
+++ BrewCask.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Check If Brew Is Installed
+if ! [ -x "$(command -v brew)" ]; then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+   else
+     brew update
+     brew upgrade
+   fi
+
+# Install Need Software on Mac #
+brew cask install evernote
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install google-chrome
+brew cask install peakhour
+brew cask install dropbox
+brew cask install telegram
+brew cask install 1password
+brew cask install iterm2
+brew cask install vmware-fusion
+brew cask install ipvanish-vpn
+brew cask install teamviewer
